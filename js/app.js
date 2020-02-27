dca0b2136109dbd9c7937f366f0f82f8044ddda2
Dragos30 <49732388+Dragos30@users.noreply.github.com>
2020-02-27 13:33:40 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,505 @@
+    function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+   
+  }
+   
+   function addItemToCart1() {
+    var a = document.getElementsByClassName("card")[0];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[0];
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    c.className="clonePrice";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem");
+     if (x.style.display === "inline") {
+       x.style.display = "none";
+     }
+     else if 
+       (x.style.display = "none");
+    }
+
+   function addItemToCart2() {
+    var a = document.getElementsByClassName("card")[1];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[1];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem2");
+     if (x.style.display === "inline") {
+       x.style.display = "none";
+     }
+     else if 
+       (x.style.display = "none");
+     
+    }
+   
+   function addItemToCart3() {
+    var a = document.getElementsByClassName("card")[2];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[2];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem3");
+     if (x.style.display === "inline") {
+       x.style.display = "none";
+     }
+     else if 
+       (x.style.display = "none");
+   }
+
+   function addItemToCart4() {
+    var a = document.getElementsByClassName("card")[3];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[3];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem4");
+     if (x.style.display === "inline") {
+       x.style.display = "none";
+     }
+     else if 
+       (x.style.display = "none");
+   }
+   function addItemToCart5() {
+    var a = document.getElementsByClassName("card")[4];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[4];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem5");
+     if (x.style.display === "inline") {
+       x.style.display = "none";
+     }
+     else if 
+       (x.style.display = "none");
+   }
+   function addItemToCart6() {
+    var a = document.getElementsByClassName("card")[5];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[5];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem6");
+     if (x.style.display === "inline") {
+       x.style.display = "none";
+     }
+     else if 
+       (x.style.display = "none");
+   }
+   
+   function addItemToCart7() {
+    var a = document.getElementsByClassName("card")[6];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[6];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem7");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+  }
+   
+   function addItemToCart8() {
+    var a = document.getElementsByClassName("card")[7];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[7];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?")
+    var x = document.getElementById("addItem8");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+
+   function addItemToCart9() {
+    var a = document.getElementsByClassName("card")[8];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[8];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem9");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+
+   function addItemToCart10() {
+    var a = document.getElementsByClassName("card")[9];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[9];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem10");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+
+   function addItemToCart11() {
+    var a = document.getElementsByClassName("card")[11];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[10];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem11");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart12() {
+    var a = document.getElementsByClassName("card")[13];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[11];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem12");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart13() {
+    var a = document.getElementsByClassName("card")[14];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[12];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem13");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart14() {
+    var a = document.getElementsByClassName("card")[15];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[13];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem14");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart15() {
+    var a = document.getElementsByClassName("card")[16];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[14];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem15");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart16() {
+    var a = document.getElementsByClassName("card")[17];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[15];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem16");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart17() {
+    var a = document.getElementsByClassName("card")[18];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[16];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem17");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart18() {
+    var a = document.getElementsByClassName("card")[19];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[17];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem18");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart19() {
+    var a = document.getElementsByClassName("card")[20];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[18];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem19");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart20() {
+    var a = document.getElementsByClassName("card")[21];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[19];
+    c.className = "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem20");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart21() {
+    var a = document.getElementsByClassName("card")[22];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[20];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem21");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart22() {
+    var a = document.getElementsByClassName("card")[23];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[21];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem22");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart23() {
+    var a = document.getElementsByClassName("card")[24];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[22];
+    c.className="clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem23");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart24() {
+    var a = document.getElementsByClassName("card")[25];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[23];
+    c.className = "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem24");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart25() {
+    var a = document.getElementsByClassName("card")[26];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[24];
+    c.className = "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem25");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart26() {
+    var a = document.getElementsByClassName("card")[27];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[25];
+    c.className = "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem26");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart27() {
+    var a = document.getElementsByClassName("card")[28];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[26];
+    c.className = "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem27");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart28() {
+    var a = document.getElementsByClassName("card")[29];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[27];
+    c.className = "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem28");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart29() {
+    var a = document.getElementsByClassName("card")[30];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[28];
+    c.className= "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem29");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   }
+   function addItemToCart30() {
+    var a = document.getElementsByClassName("card")[31];
+    var b = a.cloneNode(true);
+    var c = document.getElementsByClassName("cardPrice")[29];
+    c.className= "clonePrice";
+    document.getElementById("part3").appendChild(b);
+    b.className = "cardClone";
+    alert("Add painting to your cart?");
+    var x = document.getElementById("addItem30");
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    }
+    else if 
+      (x.style.display = "none");
+   } 
+
+      function popUp(){
+      var sum = 0.0;
+     $('.clonePrice').each(function popUp()
+     {
+        sum += parseFloat($(this).text());
+     });;
+        $("#sum").text("Total amount to pay is :" + sum+ "$")
+      }
+
+       $(document).ready(function(){
+        $("#complete").click(function(){
+        $("#part5").toggle(1000);
+        });
+      });
+
+        $(document).ready(function(){
+          $("#calc").click(function(){
+          $("#sum").toggle(1000);
+          });
+        });
+
+
+        var itemCount = 0;
+      $('.item_add').click(function (){
+        itemCount ++;
+        $('#itemCount').html(itemCount).css('display', 'block');
+      }); 
+
+      $('.clear').click(function() {
+        itemCount = 0;
+        $('#itemCount').html('').css('display', 'none');
+        $('.cardClone').html('').each;
+      }); 
+ 
+
+  
